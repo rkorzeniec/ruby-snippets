14bd1022d412dc779771f8f9e51b4e2f22316bcb
Robert Korzeniec <r.korzeniec@gmail.com>
2022-02-27 17:25:34 +0100

    Create 1.2_reverse_string.rb

---
+++ daily_byte/1.2_reverse_string.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class Reverser
+  def initialize(text)
+    @text = text
+  end
+
+  def call
+    text.reverse
+  end
+
+  private
+
+  attr_reader :text
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    assert_equal 'taC', Reverser.new('Cat').call
+  end
+
+  def test_2
+    assert_equal 'etyB yliaD ehT', Reverser.new('The Daily Byte').call
+  end
+
+  def test_3
+    assert_equal 'civic', Reverser.new('civic').call
+  end
+end
