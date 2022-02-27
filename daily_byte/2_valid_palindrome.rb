d50b1de919861ebc706d91bad485b7ac1ae4055d
Robert Korzeniec <r.korzeniec@gmail.com>
2022-02-27 18:09:37 +0100

    Create 2_valid_palindrome.rb

---
+++ daily_byte/2_valid_palindrome.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# This question is asked by Facebook.
+# Given a string, return whether or not it forms a palindrome ignoring case and
+# non-alphabetical characters.
+
+# NOTE: a palindrome is a sequence of characters that reads the same forwards
+# and backwards.
+
+require 'minitest/autorun'
+
+class Palindrome
+  def initialize(text)
+    @text = sanitized_text(text)
+  end
+
+  def check?
+    front = 0
+    back = text_length
+
+    while front < back
+      return false unless text[front] == text[back]
+
+      front += 1
+      back -= 1
+    end
+
+    true
+  end
+
+  private
+
+  attr_reader :text
+
+  def sanitized_text(text)
+    text.gsub(/[^a-zA-Z]/, '').downcase
+  end
+
+  def text_length
+    text.length - 1
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    assert_equal true, Palindrome.new('level').check?
+  end
+
+  def test_2
+    assert_equal false, Palindrome.new('algorithm').check?
+  end
+
+  def test_3
+    assert_equal true, Palindrome.new('A man, a plan, a canal: Panama.').check?
+  end
+end
