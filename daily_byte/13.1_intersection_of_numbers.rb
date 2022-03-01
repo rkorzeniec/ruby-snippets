30dfcd457ad606ac34aae8ddd868399445bd100a
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-01 09:56:59 +0100

    Create 13.1_intersection_of_numbers.rb

---
+++ daily_byte/13.1_intersection_of_numbers.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# This question is asked by Google.
+# Given two integer arrays, return their intersection.
+# NOTE: the intersection is the set of elements that are common to both arrays.
+
+require 'minitest/autorun'
+
+class ArrayIntersection
+  def initialize(numbers)
+    @numbers = numbers
+  end
+
+  def find(other_numbers)
+    numbers.intersection(other_numbers)
+  end
+
+  private
+
+  attr_reader :numbers
+end
+
+class TestArrayIntersection < Minitest::Test
+  def test_1
+    assert_equal [2, 4], ArrayIntersection.new([2, 4, 4, 2]).find([2, 4])
+  end
+
+  def test_2
+    assert_equal [3], ArrayIntersection.new([1, 2, 3, 3]).find([3, 3])
+  end
+
+  def test_3
+    assert_equal [], ArrayIntersection.new([2, 4, 6, 8]).find([1, 3, 5, 7])
+  end
+end
