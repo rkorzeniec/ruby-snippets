fc25641fda646982c95e383cb89ab7c6f037f2d0
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-03 08:20:28 +0100

    Create 22_validate_characters.rb

---
+++ daily_byte/22_validate_characters.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+# This question is asked by Google.
+# Given a string only containing the following characters (, ), {, }, [, and ]
+# return whether or not the opening and closing characters are in a valid order.
+#
+# "(){}[]", return true
+# "(({[]}))", return true
+# "{(})", return false
+
+require 'minitest/autorun'
+
+class Brackets
+  PARENTHESIS = { ')' => '(', ']' => '[', '}' => '{' }.freeze
+
+  def initialize(input)
+    @input = input
+    @stack = []
+  end
+
+  def valid?
+
+    input.chars do |char|
+      if opening_parenthesis.include?(char)
+        stack.append(char)
+      elsif invalid_input?(char)
+        return false
+      end
+    end
+
+    stack.size.zero?
+  end
+
+  private
+
+  attr_reader :input, :stack
+
+  def opening_parenthesis
+    @opening_parenthesis ||= PARENTHESIS.values
+  end
+
+  def closing_parenthesis
+    @closing_parenthesis ||= PARENTHESIS.keys
+  end
+
+  def invalid_input?(char)
+    return true if stack.size.zero?
+
+    closing_parenthesis.include?(char) && PARENTHESIS[char] != stack.pop
+  end
+end
+
+class TestBrackets < Minitest::Test
+  def test_1
+    assert_equal true, Brackets.new('(){}[]').valid?
+  end
+
+  def test_2
+    assert_equal true, Brackets.new('(({[]}))').valid?
+  end
+
+  def test_3
+    assert_equal false, Brackets.new('{(})').valid?
+  end
+end
