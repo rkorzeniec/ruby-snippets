39177019e71780c62010d4eda07c8b0725c5dcbd
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-05 20:03:34 +0100

    Create 217.2_contains_duplicate.rb

---
+++ leet_code/217.2_contains_duplicate.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/contains-duplicate/
+
+require 'minitest/autorun'
+
+# Runtime: 104 ms, faster than 84.37% of Ruby online submissions for Contains Duplicate.
+# Memory Usage: 218.9 MB, less than 44.37% of Ruby online submissions for Contains Duplicate.
+class Solution
+  class << self
+    def contains_duplicate(nums)
+      cache = {}
+
+      nums.each do |num|
+        return true if cache[num]
+
+        cache[num] = true
+      end
+
+      false
+    end
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    assert_equal true, Solution.contains_duplicate([1, 2, 3, 1])
+  end
+
+  def test_2
+    assert_equal false, Solution.contains_duplicate([1, 2, 3, 4])
+  end
+
+  def test_3
+    assert_equal true, Solution.contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
+  end
+end
