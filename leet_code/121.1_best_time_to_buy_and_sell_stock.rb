4b328460d9e3dfb155c3b323139467ea4b500720
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-06 14:03:30 +0100

    Create 121.1_best_time_to_buy_and_sell_stock.rb

---
+++ leet_code/121.1_best_time_to_buy_and_sell_stock.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+require 'minitest/autorun'
+
+# Runtime: 245 ms, faster than 52.51% of Ruby online submissions for Best Time to Buy and Sell Stock.
+# Memory Usage: 220.7 MB, less than 60.66% of Ruby online submissions for Best Time to Buy and Sell Stock.
+class Solution
+  class << self
+    def max_profit(prices)
+      max_profit = 0
+      current_buy = Float::INFINITY
+      current_sell = -Float::INFINITY
+
+      prices.each do |price|
+        if price < current_buy
+          current_buy = price
+          current_sell = -Float::INFINITY
+        end
+
+        current_sell = price if price > current_sell
+
+        current_profit = current_sell - current_buy
+        max_profit = current_profit if current_profit > max_profit
+      end
+
+      max_profit
+    end
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    assert_equal 5, Solution.max_profit([7, 1, 5, 3, 6, 4])
+  end
+
+  def test_2
+    assert_equal 0, Solution.max_profit([7, 6, 4, 3, 1])
+  end
+end
