e53e23798e322864933c4d62abd8a7f80884a258
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-12 06:16:04 +0100

    Create 189.1_rotate_array.rb

---
+++ leet_code/189.1_rotate_array.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/rotate-array/
+
+require 'minitest/autorun'
+
+# Runtime: 159 ms, faster than 73.19% of Ruby online submissions for Rotate Array.
+# Memory Usage: 219 MB, less than 20.38% of Ruby online submissions for Rotate Array.
+class Solution
+  class << self
+    def rotate(nums, k)
+      k = k % nums.size
+
+      k.times do |_|
+        temp = nums.pop
+        nums.prepend(temp)
+      end
+
+      nums
+    end
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    output = [5, 6, 7, 1, 2, 3, 4]
+
+    assert_equal output, Solution.rotate(nums, k)
+  end
+
+  def test_2
+    nums = [-1, -100, 3, 99]
+    k = 2
+    output = [3, 99, -1, -100]
+
+    assert_equal output, Solution.rotate(nums, k)
+  end
+end
