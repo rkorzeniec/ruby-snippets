045e6ef7b38ae3d049dd7c35909bba7ac2e1e1e8
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-12 06:16:18 +0100

    Create 278.1_first_bad_version.rb

---
+++ leet_code/278.1_first_bad_version.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/first-bad-version/
+
+require 'minitest/autorun'
+
+# Runtime: 80 ms, faster than 66.99% of Ruby online submissions for First Bad Version.
+# Memory Usage: 210.7 MB, less than 44.74% of Ruby online submissions for First Bad Version.
+class Solution
+  class << self
+    def first_bad_version(n)
+      low = 1
+
+      while low < n
+        mid = low + (n - low) / 2
+
+        if is_bad_version(mid)
+          n = mid
+        else
+          low = mid + 1
+        end
+      end
+
+      low
+    end
+  end
+end
