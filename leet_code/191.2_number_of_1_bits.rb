49c0a965c8b8885fe1052ef26cbba0fd1be8b0f1
Robert Korzeniec <r.korzeniec@gmail.com>
2022-03-14 06:12:06 +0100

    Create 191.2_number_of_1_bits.rb

---
+++ leet_code/191.2_number_of_1_bits.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/number-of-1-bits/
+
+require 'minitest/autorun'
+
+# Runtime: 107 ms, faster than 49.59% of Ruby online submissions for Number of 1 Bits.
+# Memory Usage: 210.9 MB, less than 17.07% of Ruby online submissions for Number of 1 Bits.
+class Solution
+  class << self
+    def hamming_weight(n)
+      count = 0
+
+      while n > 0
+        count += n & 1
+        n >>= 1
+      end
+
+      count
+    end
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    n = 00000000000000000000000000001011
+    output = 3
+
+    assert_equal output, Solution.hamming_weight(n)
+  end
+
+  def test_2
+    n = 00000000000000000000000010000000
+    output = 1
+
+    assert_equal output, Solution.hamming_weight(n)
+  end
+
+  def test_3
+    n = 11111111111111111111111111111101
+    output = 31
+
+    assert_equal output, Solution.hamming_weight(n)
+  end
+end
