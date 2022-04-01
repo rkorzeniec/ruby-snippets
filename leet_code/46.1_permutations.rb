1e47365c723d07b531e2b400f873ea18adbb7047
Robert Korzeniec <r.korzeniec@gmail.com>
2022-04-01 10:32:07 +0200

    Create 46.1_permutations.rb

---
+++ leet_code/46.1_permutations.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# https://leetcode.com/problems/permutations/
+
+require_relative '../test_helper'
+
+
+class Solution
+  class << self
+    def permute(nums)
+      @permutations = []
+      backtrack(nums:, combination: [])
+      @permutations
+    end
+
+    def backtrack(nums:, combination:)
+      return @permutations.append(combination.dup) if nums.empty?
+
+      nums.size.times do |i|
+        other_nums = nums[...i] + nums[(i + 1)..]
+
+        combination.append(nums[i])
+        backtrack(nums: other_nums, combination:)
+        combination.pop
+      end
+    end
+  end
+end
+
+class TestSolution < Minitest::Test
+  def test_1
+    nums = [1, 2, 3]
+    output = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+
+    assert_equal output, Solution.permute(nums)
+  end
+
+  def test_2
+    nums = [0, 1]
+    output = [[0, 1], [1, 0]]
+
+    assert_equal output, Solution.permute(nums)
+  end
+
+  def test_3
+    nums = [1]
+    output = [[1]]
+
+    assert_equal output, Solution.permute(nums)
+  end
+end
